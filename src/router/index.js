92b65ce5e5e1e8877a67e318b9c7e962b7d58a14
xiwuqi <qiyuewuxi@yeah.net>
2023-08-22 02:36:00 +0800

    基本完成左侧导航栏，新增文章管理、日志中心、用户管理、公告管理、标签管理、分类管理等

--- src/router/index.js
+++ src/router/index.js
@@ -56,6 +56,28 @@ export const constantRoutes = [
   },
 
   {
+    path: '/articles',
+    component: Layout,
+    redirect: '/articles/list',
+    name: 'Articles',
+    meta: { title: '文章管理', icon: 'wz' },
+    children: [
+      {
+        path: 'list',
+        name: 'ArticlesList',
+        component: () => import('@/views/articles/list'),
+        meta: { title: '文章列表', icon: 'wenzhang' }
+      },
+      {
+        path: 'add',
+        name: 'Addrticles',
+        component: () => import('@/views/articles/add'),
+        meta: { title: '发布文章', icon: 'fabu' }
+      }
+    ]
+  },
+
+  {
     path: '/operation',
     component: Layout,
     redirect: '/operation/list',
@@ -73,6 +95,82 @@ export const constantRoutes = [
         name: 'LoginLogList',
         component: () => import('@/views/operation/loginlog'),
         meta: { title: '登录日志', icon: '' }
+      }
+    ]
+  },
+
+  {
+    path: '/user',
+    component: Layout,
+    redirect: '/user/list',
+    name: 'User',
+    meta: { title: '用户管理', icon: 'user' },
+    children: [
+      {
+        path: 'list',
+        name: 'UserList',
+        component: () => import('@/views/user/list'),
+        meta: { title: '用户列表', icon: 'user' }
+      },
+      {
+        path: 'introduction',
+        name: 'Introduction',
+        component: () => import('@/views/user/introduction'),
+        meta: { title: '个人介绍', icon: 'jieshao' }
+      }
+    ]
+  },
+
+  {
+    path: '/notice',
+    component: Layout,
+    redirect: '/notice/list',
+    name: 'Notice',
+    meta: { title: '公告管理', icon: 'notice' },
+    children: [
+      {
+        path: 'list',
+        name: 'NoticeList',
+        component: () => import('@/views/notice/list'),
+        meta: { title: '公告列表', icon: 'notice' }
+      },
+      {
+        path: 'add',
+        name: 'AddNotice',
+        component: () => import('@/views/notice/add'),
+        meta: { title: '发布公告', icon: 'fabu' }
+      }
+    ]
+  },
+
+  {
+    path: '/tag',
+    component: Layout,
+    redirect: '/tag/list',
+    name: 'Tag',
+    meta: { title: '标签管理', icon: 'biaoqian' },
+    children: [
+      {
+        path: 'list',
+        name: 'TagList',
+        component: () => import('@/views/tag/list'),
+        meta: { title: '标签管理', icon: 'biaoqian' }
+      }
+    ]
+  },
+
+  {
+    path: '/categories',
+    component: Layout,
+    redirect: '/categories/list',
+    name: 'Categories',
+    meta: { title: '分类管理', icon: 'fenlei' },
+    children: [
+      {
+        path: 'list',
+        name: 'CategoriesList',
+        component: () => import('@/views/categories/list'),
+        meta: { title: '分类管理', icon: 'fenlei' }
       }
     ]
   },
