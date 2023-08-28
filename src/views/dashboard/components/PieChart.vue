f7504e319f35ab6e893ae25afa3fb9999d0f979b
xiwuqi <qiyuewuxi@yeah.net>
2023-08-28 13:43:44 +0800

    实现文章分类占比组件

---
+++ src/views/dashboard/components/PieChart.vue
@@ -0,0 +1,90 @@
+<template>
+  <div :class="className" :style="{height:height,width:width}" />
+</template>
+
+<script>
+import * as echarts from 'echarts'
+import { getCategoryCount } from '@/api/index'
+require('echarts/theme/macarons') // echarts theme
+
+export default {
+  props: {
+    className: {
+      type: String,
+      default: 'chart'
+    },
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '300px'
+    }
+  },
+  data() {
+    return {
+      chart: null,
+      list: []
+    }
+  },
+  mounted() {
+    getCategoryCount().then(res => {
+      if (res.code === 200) {
+        var getData = []
+        for (let i = 0; i < res.data.length; i++) {
+          var obj = new Object()
+          obj.name = res.data[i].categoryName
+          obj.value = res.data[i].categoryCount
+          getData[i] = obj
+        }
+        this.list = getData
+        this.initChart()
+      } else {
+        this.$message({
+          type: 'error',
+          message: res.msg
+        })
+      }
+    })
+  },
+
+  beforeDestroy() {
+    if (!this.chart) {
+      return
+    }
+    this.chart.dispose()
+    this.chart = null
+  },
+  methods: {
+    initChart() {
+      this.chart = echarts.init(this.$el, 'macarons')
+
+      this.chart.setOption({
+        title: {
+          text: '分类占比',
+          left: 'left'
+        },
+        tooltip: {
+          trigger: 'item'
+        },
+        series: [
+          {
+            type: 'pie',
+            radius: '50%',
+            data: this.list,
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+
+      })
+    }
+  }
+}
+</script>
