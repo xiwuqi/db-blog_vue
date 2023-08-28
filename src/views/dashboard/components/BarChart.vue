8d9f59bd328eabe1fb5ca4ec8c976cb47dedd647
xiwuqi <qiyuewuxi@yeah.net>
2023-08-28 13:40:20 +0800

    实现近一周发文数量统计组件

---
+++ src/views/dashboard/components/BarChart.vue
@@ -0,0 +1,103 @@
+<template>
+  <div :class="className" :style="{height:height,width:width}" />
+</template>
+
+<script>
+import * as echarts from 'echarts'
+import { getWeekArticleCount } from '@/api/index'
+
+require('echarts/theme/macarons') // echarts theme
+
+export default {
+  props: {
+    className: {
+      type: String,
+      default: 'chart'
+    },
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '300px'
+    }
+  },
+
+  data() {
+    return {
+      chart: null,
+      list: []
+    }
+  },
+
+  mounted() {
+    getWeekArticleCount().then(res => {
+      if (res.code === 200) {
+        this.list = res.data
+        this.initChart()
+      } else {
+        this.$message({
+          type: 'error',
+          message: res.msg
+        })
+      }
+    })
+  },
+
+  beforeDestroy() {
+    if (!this.chart) {
+      return
+    }
+    this.chart.dispose()
+    this.chart = null
+  },
+
+  methods: {
+    initChart() {
+      this.chart = echarts.init(this.$el, 'macarons')
+
+      this.chart.setOption({
+        title: {
+          text: '发文数量'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow'
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            type: 'category',
+            data: this.list.map(item => item.date),
+            axisTick: {
+              alignWithLabel: true
+            }
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ],
+        series: [
+          {
+            name: '文章数',
+            type: 'bar',
+            barWidth: '60%',
+            data: this.list.map(item => item.count)
+          }
+        ]
+
+      })
+    }
+  }
+}
+</script>
